38f159d28ee85a8012917a441c65bdb45a679510
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-03-24 15:46:51 +0900

    練習問題11

---
+++ server/src/sample/L11.java
@@ -0,0 +1,22 @@
+package sample;
+
+class L11{
+    
+    public static void main (String args[]){
+        int x, y, ans;
+        x = 10;
+        y = 3;
+        ans = x + y;
+        System.out.println("X=" + x + ",Y=" + y +" (和) --> " + ans);
+        ans = x - y;
+        System.out.println("X=" + x + ",Y=" + y +" (差) --> " + ans);
+        ans = x * y;
+        System.out.println("X=" + x + ",Y=" + y +" (積) --> " + ans);
+        ans = x / y;
+        System.out.println("X=" + x + ",Y=" + y +" (商) --> " + ans);
+        ans = x % y;
+        System.out.println("X=" + x + ",Y=" + y +" (剰余) --> " + ans);
+    }
+    
+    
+}
