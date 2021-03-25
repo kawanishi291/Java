9199d2219a07dfc39895fc3abb80b3c6f1c44a57
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-03-25 14:36:23 +0900

    子供の身長計算(バグ修正)

--- server/src/ChildHeight.java
+++ server/src/ChildHeight.java
@@ -1,20 +1,16 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ChildHeight {
 
 
-    private static List<Double> SetHeight(){
+    private static double[] SetHeight(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("父親の身長(cm)：");
         double f = scanner.nextDouble();
         System.out.print("母親の身長(cm)：");
         double m = scanner.nextDouble();
         scanner.close();
-        List<Double> values = new ArrayList();
-        values.add(f);
-        values.add(m);
+        double values[] = {f, m};
 
         return values;
     }
@@ -22,16 +18,17 @@ public class ChildHeight {
     private static double[] CalcHeight(double f, double m){
         double boy = (f + m + 13) / 2 + 2;
         double girl = (f + m - 13) / 2 + 2;
-        double[] values = {boy, girl};
+        double values[] = {boy, girl};
 
         return values;
     }
 
 
     public static void main(String[] args) {
-        List<Double> list = SetHeight();
-        double farther = list.get(0);
-        double mother = list.get(1);
+        System.out.println("子供の身長を予測します。");
+        double list[] = SetHeight();
+        double farther = list[0];
+        double mother = list[1];
         double[] values = CalcHeight(farther, mother);
         int i = 0;
         for (double value: values){
