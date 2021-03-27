28fb107a7482404421653e2dfb6c230a59cf2018
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-03-28 01:25:33 +0900

    練習42

---
+++ server/src/sample/L42.java
@@ -0,0 +1,26 @@
+package sample;
+
+class Cash {
+
+    public static void main(String[] args) {
+        BankAccount obj;
+        obj = new BankAccount();
+        obj.setBalance(10000);
+        obj.withdrawBalance(1000);
+    }
+}
+
+class BankAccount {
+    int balance; //残高
+    
+    void setBalance (int price){
+        balance = price;
+    }
+    void withdrawBalance (int price){
+        if((balance - price) > 0) {
+            balance -= price;
+            System.out.println("残高は：" + balance + "円です。");
+        } else 
+            System.out.println("残高が足りません。");
+    }
+}
