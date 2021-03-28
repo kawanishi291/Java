d2eb92ee4a2722a9a94c43755451c8da85f299a1
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-03-28 14:07:12 +0900

    練習48

---
+++ server/src/sample/L48.java
@@ -0,0 +1,32 @@
+package sample;
+
+class SuperPerson {
+    String name;
+    int age;
+    
+    void outPerson() {
+        System.out.println("名前：" + this.name);
+        System.out.println("年齢：" + this.age);
+    }
+}
+
+class SubStudents extends SuperPerson {
+    String className;
+
+    void outPerson() {
+
+        super.outPerson();
+        System.out.println("クラス：" + this.className);
+    }
+}
+
+class OverRideTest {
+
+    public static void main(String[] args) {
+        SubStudents obj = new SubStudents();
+        obj.name = "田中";
+        obj.age = 27;
+        obj.className = "1 - 1";
+        obj.outPerson();
+    }
+}
